cd1d7e9e7d8c26e4217f0a94a679b09d15a00c81
JulioCampos-Z5 <julioz5435@gmail.com>
2025-09-16 18:24:08 -0600

    Refactor code structure for improved readability and maintainability

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable {
+class User extends Authenticatable
+{
     use Notifiable;
-    protected $fillable = ['name','email','password','role'];
+    protected $fillable = ['name', 'email', 'password', 'role'];
     protected $hidden = ['password'];
 }
